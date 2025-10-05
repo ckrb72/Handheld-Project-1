0e37d406b8f5e81c2f49e2db9f2b6b0b38112daa
Ciaran Krabak <ckr26@Ciarans-MacBook-Pro.local>
2025-10-05 16:58:49 -0400

    Setup layout for search page

--- app/src/main/java/com/example/project1/MainSearchActivity.kt
+++ app/src/main/java/com/example/project1/MainSearchActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.project1.ui.theme.Project1Theme
 
 class MainSearchActivity : ComponentActivity() {
@@ -19,7 +33,7 @@ class MainSearchActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Project1Theme {
-                SearchView()
+                SearchView(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -27,16 +41,60 @@ class MainSearchActivity : ComponentActivity() {
 
 @Composable
 fun SearchView(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello",
-        modifier = modifier
-    )
+
+    var searchTerm by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.height(IntrinsicSize.Max)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                TextField(
+                    value = searchTerm,
+                    onValueChange = {newValue -> searchTerm = newValue},
+                    singleLine = true,
+                    label = { Text("Search Terms") },
+                    modifier = Modifier.fillMaxHeight().padding(0.dp, 8.dp, 0.dp, 8.dp)
+                )
+            }
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(5.dp, 0.dp, 0.dp, 0.dp)
+            ) {
+                Button(
+                    onClick = {
+
+                    }
+                ) {
+                    Text("Filter")
+                }
+
+                Button(
+                    enabled = searchTerm.isNotBlank(),
+                    onClick = {
+
+                    }
+                ) {
+                    Text("Search")
+                }
+            }
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun SearchPreview() {
     Project1Theme {
-        SearchView()
+        SearchView(modifier = Modifier.fillMaxSize())
     }
 }
