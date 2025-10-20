ad85b9236939d1ea71458047f7304836a4de128d
Ciaran Krabak <krck70153@gmail.com>
2025-10-19 22:36:13 -0400

    added article list when long clicking on map

--- app/src/main/java/com/example/project1/MapsActivity.kt
+++ app/src/main/java/com/example/project1/MapsActivity.kt
@@ -1,14 +1,29 @@
 package com.example.project1
 
+import android.content.Intent
 import android.location.Address
 import android.location.Geocoder
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,9 +32,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.project1.ui.theme.Project1Theme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -60,9 +79,10 @@ suspend fun getAddressGeocodeCurrent(context: android.content.Context, latLng: L
                 override fun onGeocode(addressList: MutableList<Address>) {
                     val result = if (addressList.isNotEmpty()) {
                         val address = addressList[0]
-                        val city = address.locality ?: "Unknown City"
+                        //val city = address.locality ?: "Unknown City"
                         val state = address.adminArea ?: "Unknown State"
-                        "$city, $state"
+                        val country = address.countryName ?: "Unknown Country"
+                        "$state, $country"
                     } else {
                         "No address found."
                     }
@@ -93,24 +113,95 @@ fun MapsView(modifier: Modifier = Modifier) {
             }
         }
     }
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-        onMapLongClick = { latLng ->
-            markerPosition = latLng
-            addressInfo = "Resolving address..."
-        }
-    ) {
-        markerPosition?.let { position ->
-            Marker(state = MarkerState(position = position),
-                title = addressInfo,
-                snippet = "Lat: $position.latitude Lng: $position.longitude"
-            )
 
-            Log.d("ADDRESS", addressInfo)
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+            onMapLongClick = { latLng ->
+                markerPosition = latLng
+                addressInfo = "Resolving address..."
+            }
+        ) {
+            markerPosition?.let { position ->
+                Marker(state = MarkerState(position = position),
+                    title = addressInfo,
+                    snippet = "Lat: $position.latitude Lng: $position.longitude"
+                )
+
+                Log.d("ADDRESS", addressInfo)
+            }
+        }
+
+        val fakeArticles = getFakeData()
+
+        markerPosition?.let {
+            Card(
+                modifier = Modifier.fillMaxWidth(0.85f)
+                    .fillMaxHeight(0.25f)
+                    .padding(0.dp, 0.dp, 0.dp, 25.dp)
+            ) {
+                LazyRow(
+                    modifier = Modifier.fillMaxSize()
+                        .padding(10.dp)
+                ) {
+                    items(fakeArticles) { article ->
+                        ArticleRowCard(article) {
+                            val intent = Intent(Intent.ACTION_VIEW).apply {
+                                data = Uri.parse(article.url)
+                            }
+                            context.startActivity(intent)
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
+@Composable
+fun ArticleRowCard(article: ArticleData, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Card(
+        modifier = modifier.fillMaxHeight()
+            .fillMaxWidth(0.25f),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        border = BorderStroke(1.dp, Color.Gray),
+        onClick = onClick
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight()
+                    .width(100.dp)
+                    .padding(10.dp)
+            ) {
+                AsyncImage(
+                    model = article.icon,
+                    contentDescription = null
+                )
+            }
+            Column(
+                modifier = Modifier.fillMaxSize()
+                    .padding(10.dp),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(article.title)
+                Text(article.source)
+                Text(article.description)
+
+            }
+        }
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
